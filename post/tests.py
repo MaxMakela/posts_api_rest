3748ba0dd4be6aed0ec938c76bdb358d734cf009
MaxMakela <johnhopeful11@gmail.com>
2019-12-20 06:05:19 +0200

    add reg test

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from .serializers import PostSerializer, UserSerializer
+from .models import Post
+
+class RegistrationTestCase(APITestCase):
+
+    def test_registration(self):
+        data = {'username': 'name1', 'email': 'test@mail.com', 'password': 'some_password_1234'}
+        response = self.client.post('/api/users/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
